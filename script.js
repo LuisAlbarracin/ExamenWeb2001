d4ba3e9d8ebc0f118b5559c398d3d12139d17699
Luis Vicente Albarracin <luisalbarracin9727@gmail.com>
2021-04-15 06:43:02 -0500

    Primer Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const url1 = "http://demo6497253.mockable.io/noticias"
+const url2 = "http://demo6497253.mockable.io/categoria/deporte"
+const url3 = "http://demo6497253.mockable.io/categoria/tecnologia"
+const url4 = "http://demo6497253.mockable.io/noticias/1"
+const url5 = "http://demo6497253.mockable.io/noticias/6"
+
+
+function cargarDeportes(){
+    const depor = document.getElementById('deportes')
+}

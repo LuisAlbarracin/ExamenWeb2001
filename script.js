d49a231f98b3d8157cfe96ae042f9dc46ea2f8b8
Luis Vicente Albarracin <luisalbarracin9727@gmail.com>
2021-04-15 07:51:12 -0500

    Segundo Commit

--- script.js
+++ script.js
@@ -1,10 +1,65 @@
-const url1 = "http://demo6497253.mockable.io/noticias"
-const url2 = "http://demo6497253.mockable.io/categoria/deporte"
-const url3 = "http://demo6497253.mockable.io/categoria/tecnologia"
-const url4 = "http://demo6497253.mockable.io/noticias/1"
-const url5 = "http://demo6497253.mockable.io/noticias/6"
 
+//const url4 = "https://carlosreneas.github.io/endpoints/noticia_1.json";
+//const url5 = "https://carlosreneas.github.io/endpoints/noticia_6.json";
 
-function cargarDeportes(){
-    const depor = document.getElementById('deportes')
+function llenarDeportes(data){
+
+    const depor = document.getElementById("deportes")
+    depor.innerHTML += `<p>${data[0].titulo}</p><hr><p>${data[1].titulo}</p><hr><p>${data[2].titulo}</p>`
+
+    
 }
+
+function cargarDeportes() {
+
+  const url2 = "https://carlosreneas.github.io/endpoints/categoria_deporte.json";
+  fetch(url2)
+  .then((response)=>response.json())
+  .then((data)=>{
+    llenarDeportes(data)
+  })
+  .catch((err) => console.log(err))
+}
+
+function llenarTecnologia(data){
+    const tecnologia = document.getElementById("tecnologia")
+    tecnologia.innerHTML += `<p>${data[0].titulo}</p><hr><p>${data[1].titulo}</p><hr><p>${data[2].titulo}</p>`
+
+}
+
+function cargarTecnologia(){
+
+    const url3 = "https://carlosreneas.github.io/endpoints/categoria_tecnologia.json";
+    fetch(url3)
+    .then((response)=>response.json())
+    .then((data)=>{
+        llenarTecnologia(data)
+    })
+    .catch((err) => console.log(err))
+}
+
+function llenarNoticias(data){
+    const noticias = document.getElementById('noticias')
+    noticias.innerHTML += `<h3>${data[0].titulo} - Colegio - ${data[0].fecha}</h3><div class="section"><p>${data[0].descripcion}</p><a href="#">Ver Mas</a></div>
+    <h3>${data[1].titulo} - Colegio - ${data[0].fecha}</h3><div class="section"><p>${data[1].descripcion}</p><a href="#">Ver Mas</a></div>
+    <h3>${data[2].titulo} - Colegio - ${data[0].fecha}</h3><div class="section"><p>${data[2].descripcion}</p><a href="#">Ver Mas</a></div>
+    <p id="Masnoticias">Todas las noticias</p>
+    `
+}
+
+
+function cargarNoticias(){
+    const url1 = "https://carlosreneas.github.io/endpoints/noticias.json";
+    fetch(url1)
+    .then((response)=>response.json())
+    .then((data)=>{
+        console.log(data)
+        llenarNoticias(data)
+    })
+    .catch((err) => console.log(err))
+}
+
+
+cargarDeportes()
+cargarTecnologia()
+llenarNoticias()
